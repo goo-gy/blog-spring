b657338af3e796e4d65bcba9feb292b346fb30a7
goo-gy <lkukh17@naver.com>
2023-02-17 22:46:53 +0900

    init

---
+++ src/test/java/googy/blogspring/BlogSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package googy.blogspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
